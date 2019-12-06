847eab480ee61d8fa3506d8c7e6743edd7cbe560
SabineWanka <sabinewanka@yahoo.de>
2019-12-06 21:34:52 +0100

    little fixes

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var fs = require("fs");
 var Port = 3050;
 var path = require("path");
 
+// reading j.son file and storing content 
+let notes = require("./db/db.json");
+
+// defining error funktion
+var errorFn = function (err) {
+    if (err) throw err;
+};
 
 // Setting up the Express app to handle data parsing and static files 
 app.use(express.urlencoded({ extended: true }));
@@ -14,13 +21,13 @@ app.use(express.static(__dirname + "/public"));
 // getting the homepage 
 app.get('/', function (req, res) {
 
-    res.sendFile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "/index.html"))
 })
 
 // getting the notes page
 app.get('/notes', function (req, res) {
 
-    res.sendFile(path.join(__dirname, "./notes.html"))
+    res.sendFile(path.join(__dirname, "/notes.html"))
 })
 
 // getting the api for notes and display all notes
@@ -38,7 +45,7 @@ app.post("/api/notes", function (req, res) {
 });
 
 // deleting a note with an specific id 
-app.delete("/api/notes/:id", function (req, res)) {
+app.delete("./api/notes/:id", function (req, res) {
     var noteID = req.params.id;
     for (var i = 0; i < notes.length; i++) {
         if (noteID == notes[i].id) {
@@ -46,9 +53,10 @@ app.delete("/api/notes/:id", function (req, res)) {
             fs.writeFile("./db/db.json", JSON.stringify(notes), errorFn);
             return res.json(notes);
         }
+    }
+});
 
-
-        // Server listening 
-        app.listen(Port, function () {
-            console.log("App listening on port" + Port)
-        });
+// Server listening 
+app.listen(Port, function () {
+    console.log("App listening on port" + Port)
+});

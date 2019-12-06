0b99d0da94f4b1154b517be2b3b0e65260a06bfc
SabineWanka <sabinewanka@yahoo.de>
2019-12-06 21:07:34 +0100

    connectig

--- server.js
+++ server.js
@@ -1,43 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 3050
-const path = require("path")
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var Port = 3050;
+var path = require("path");
 
+
+// Setting up the Express app to handle data parsing and static files 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
-// Notes 
-
-var notes = [
-
-];
-
+// Bulidung the routes
+// getting the homepage 
 app.get('/', function (req, res) {
 
     res.sendFile(path.join(__dirname, "index.html"))
 })
 
+// getting the notes page
 app.get('/notes', function (req, res) {
 
-    res.sendFile(path.join(__dirname, "notes.html"))
+    res.sendFile(path.join(__dirname, "./notes.html"))
 })
 
-// Displays all notes
+// getting the api for notes and display all notes
 app.get("/api/notes", function (req, res) {
     return res.json(notes);
 });
 
-// Adding new notes 
+// adding a new note und giving this note a specific ID, the ID is a number between 0 and 1000
 app.post("/api/notes", function (req, res) {
-    var newNotes = req.body;
-    newNotes.routeName = newNotes.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newNotes);
-    notes.push(newNotes);
-    return res.json(NewNotes);
+    var newNote = req.body;
+    notes.push(newNote);
+    newNote.id = Math.floor(Math.random() * 1000);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), errorFn);
+    return res.json(NewNote);
 });
 
+// deleting a note with an specific id 
+app.delete("/api/notes/:id", function (req, res)) {
+    var noteID = req.params.id;
+    for (var i = 0; i < notes.length; i++) {
+        if (noteID == notes[i].id) {
+            notes.splice(i, 1);
+            fs.writeFile("./db/db.json", JSON.stringify(notes), errorFn);
+            return res.json(notes);
+        }
 
-// Server listening 
-app.listen(port, function () {
-    console.log("App listening on port" + port)
-});
+
+        // Server listening 
+        app.listen(Port, function () {
+            console.log("App listening on port" + Port)
+        });
